d484f660791fee3ecdc961f7de14f021fc325e37
TamayaThomas <tathomas4@buffs.wtamu.edu>
2024-08-30 18:25:04 -0500

    Homework1 Code File

---
+++ Homework1.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       // Given values
+        double X = 2.5;
+        double Y = 3.3;
+
+        // Liner Equation
+        double Z = (4 * (X * X) + 3 * Y);
+
+        Console.WriteLine($"X = {X}, Y = {Y}");
+
+        // The result of the equation
+        Console.WriteLine("The value of Z is: " + Z);
+    }
+}
